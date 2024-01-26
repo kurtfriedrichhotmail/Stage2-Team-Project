3aaecd7e4cd057829598c7d4ddfd592a1dfe77eb
Anh.T Pham <anh.t.pham@bellevuecollege.edu>
2024-01-26 09:48:41 -0800

    edit js file

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
+//create food object constructor
+class Food{
+    constructor(fName,fCalories){
+        this.name = fName;
+        this.calories = fCalories;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
     let foodName;
     let calories;
     let total;
+    
+    //define a FoodList array
+    let foodList = [];
+
     document.getElementById("button1").addEventListener("click", function () {
         foodName = document.getElementById("foodName").value ;
         calories = document.getElementById("calories").value ;
         console.log(foodName + "  " + calories)
+
+        //call constructor
+        const foodObject = new Food(foodName, calories);
+        //push the new object into the array
+        foodList.push(foodObject);
+        console.log("sucess");
     });
 });
+
+
+
 
 
 
