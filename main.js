924f828e482b51cf1110e549953bc6439b75e071
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2024-01-25 18:16:36 -0800

    collect data from html

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-
+    let foodName;
+    let calories;
+    let total;
     document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
+        foodName = document.getElementById("foodName").value ;
+        calories = document.getElementById("calories").value ;
+        console.log(foodName + "  " + calories)
     });
 });
+
+
+
 
